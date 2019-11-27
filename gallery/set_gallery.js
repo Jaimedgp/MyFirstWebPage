23e588ffbad49e426f3f9b3cb281589ce21ebaae
Jaimedgp <jaime.diez.gp@gmail.com>
2019-11-27 20:06:04 +0100

    Set gallery scripts beatyfull

--- gallery/set_gallery.js
+++ gallery/set_gallery.js
@@ -1,15 +1,12 @@
-function getFiles(obj, colm) {
-	// Get the modal
-	var modal = document.getElementById("myModal");
-	var modalImg = document.getElementById("img01");
-	var captionText = document.getElementById("caption");
-
+function getFiles(obj, colm, modalArr) {
+	
+	// Create a Container and add Modal when click
 	var icontainer = document.createElement('div');
 	icontainer.className = 'container';
 	icontainer.onclick = function() {
-		modal.style.display = "block";
-		modalImg.src = obj.src;
-		captionText.innerHTML = "<h2>"+obj.title+"</h2>"+
+		modalArr[0].style.display = "block";
+		modalArr[1].src = obj.src;
+		modalArr[2].innerHTML = "<h2>"+obj.title+"</h2>"+
 								"<p>"+obj.description+
 									"<div align='right'>"+obj.place+", "+obj.date+"</div>"
 								+"</p>";
@@ -21,13 +18,10 @@ function getFiles(obj, colm) {
 	iImg.src = obj.src;
 	iImg.style = "width:100%";
 
+	// Create overlay to add Title
 	var iOverlay = document.createElement('div');
 	iOverlay.className = "overlay";
 	iOverlay.innerHTML = obj.title;
-
-	/*
-	iText.innerHTML = obj.description;
-	*/
 
 	// The variable icontainer is still good... Just append to it.
 	icontainer.appendChild(iImg);
@@ -45,27 +39,39 @@ function createColumns(json) {
 	for (i=0; i < numColumns; i++) {
 		columns[i] = document.createElement('div');
 		columns[i].className = 'column';
+
+		mainRow.appendChild(columns[i]);
 	}
 
-	for (i=0; i<numRows; i++) {
+	fillColumn(columns, numRows, json);
+}
 
-		for (j=numColumns*i; j<numColumns*(i+1); j++) {
+
+function fillColumn(colArr, numRows, json) {
+	// Get the modal
+	var modal = document.getElementById("myModal");
+	var modalImg = document.getElementById("img01");
+	var captionText = document.getElementById("caption");
+
+	var modalArray = [modal, modalImg, captionText];
+
+	for (i=0; i<numRows; i++) {
+		for (j=colArr.length*i; j<colArr.length*(i+1); j++) {
 			if (j < json.length) {
-				getFiles(json[j], columns[j-numColumns*i]);
+				getFiles(json[j],
+					     colArr[j-colArr.length*i],
+					     modalArray
+				        );
 			} else {
 				break;
 			}
 		}
-
-	}
-
-	for (i=0; i < numColumns; i++) {
-		mainRow.appendChild(columns[i]);
 	}
 }
 
 
-function openJSON(path) {
+function openGallery() {
+	var path = 'https://raw.githubusercontent.com/Jaimedgp/jaimedgp.github.io/master/gallery/museum.json';
 	fetch(path)
 		.then(function(data) {
 			return data.json();
@@ -75,7 +81,4 @@ function openJSON(path) {
 		})
 }
 
-var path = 'https://raw.githubusercontent.com/Jaimedgp/jaimedgp.github.io/master/gallery/museum.json'
-openJSON(path);
-
-
+openGallery();
