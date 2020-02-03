e46b9ea9c0fe007f23243416aea797e5aaf9d50f
Jaimedgp <jaime.diez.gp@gmail.com>
2020-02-03 18:23:22 +0100

    Text size by browser size

--- scroll.js
+++ scroll.js
@@ -7,14 +7,14 @@ function showDescription () {
 
 	if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
 		//title.style.textAlign = "left";
-		title.style.fontSize = "1000%";
+		title.style.fontSize = "15vh";
 		title.style.marginTop = "50px";
 
 		description.innerHTML = "Hola a todos como estamos supercalifragilisticoespialidoso"
 
 	}else {
 		//title.style.textAlign = "center";
-		title.style.fontSize = "1750%";
+		title.style.fontSize = "25vh";
 		title.style.marginTop = "100px";
 
 		description.innerHTML = "Physicist and MSc Data Science student"
