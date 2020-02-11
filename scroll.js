57622a547af2a492deca084c577dd64805bfad7a
Jaimedgp <jaime.diez.gp@gmail.com>
2020-02-11 18:22:40 +0100

    stupid change

--- scroll.js
+++ scroll.js
@@ -6,30 +6,30 @@ var lastScrollTop = 0;
 
 function showDescription () {
 
-	var st = window.pageYOffset || document.documentElement.scrollTop;
+    var st = window.pageYOffset || document.documentElement.scrollTop;
 
-	if (st > 300) {
+    if (st > 300) {
 
-		if (st > lastScrollTop){
-			if (ndiv < sections.length-1 && ndiv >= 0) {
-				var hash = "#"+sections[ndiv];
-				$('html, body').animate({ scrollTop: $(hash).offset().top }, 800, 
-							function(){ window.location.hash = hash; });
-			}
-			ndiv++;
+        if (st > lastScrollTop){
+            if (ndiv < sections.length-1 && ndiv >= 0) {
+                var hash = "#"+sections[ndiv];
+                $('html, body').animate({ scrollTop: $(hash).offset().top }, 800, 
+                            function(){ window.location.hash = hash; });
+            }
+            ndiv++;
 
-		}else {
-			if (ndiv < sections.length && ndiv > 0) {
+        }else {
+            if (ndiv < sections.length && ndiv > 0) {
 
-				var hash = "#"+sections[ndiv];
-				$('html, body').animate({ scrollTop: $(hash).offset().top }, 800, 
-							function(){ 
-								window.location.hash = sections[ndiv]; 
-							});
-			}
-			ndiv--;
-		}
+                var hash = "#"+sections[ndiv];
+                $('html, body').animate({ scrollTop: $(hash).offset().top }, 800, 
+                            function(){ 
+                                window.location.hash = sections[ndiv]; 
+                            });
+            }
+            ndiv--;
+        }
 
-		lastScrollTop = st <= 0 ? 0 : st;
-	}
+        lastScrollTop = st <= 0 ? 0 : st;
+    }
 }
